df6b5d75e3b7f6c253d9501a058f780a6ecbb957
Mona Moxie <moxie4lyf@gmail.com>
2025-05-15 19:41:19 +0200

    project initialisation

---
+++ src/main/java/com/optic/console/OpticConsoleApplication.java
@@ -0,0 +1,13 @@
+package com.optic.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpticConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpticConsoleApplication.class, args);
+	}
+
+}
