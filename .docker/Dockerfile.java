f53f8f0a0b1f1db9bfb5757948a6af482ff45c92
Mona Moxie <moxie4lyf@gmail.com>
2025-10-23 20:22:28 +0100

    refactor docker services

--- .docker/Dockerfile.java
+++ .docker/Dockerfile.java
@@ -1,5 +1,19 @@
-FROM openjdk:21-jdk-slim
+FROM maven:3.9.9-eclipse-temurin-21 AS build
 WORKDIR /app
-COPY target/optic-console.jar optic-console.jar
+
+COPY pom.xml .
+
+RUN mvn dependency:go-offline
+
+COPY src ./src
+RUN mvn clean package -DskipTests
+
+FROM eclipse-temurin:21-jdk
+WORKDIR /app
+
+COPY --from=build /app/target/*.jar app.jar
+
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "optic-console.jar"]
+
+# Start the Spring Boot app
+ENTRYPOINT ["java", "-jar", "app.jar"]
