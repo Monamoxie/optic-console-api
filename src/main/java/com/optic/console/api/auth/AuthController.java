f68526d52ef5b4d90cd47c6929201c689282b2ca
Mona Moxie <moxie4lyf@gmail.com>
2025-10-29 21:17:39 +0000

    update dockerfile

--- src/main/java/com/optic/console/api/auth/AuthController.java
+++ src/main/java/com/optic/console/api/auth/AuthController.java
@@ -1,12 +1,16 @@
 package com.optic.console.api.auth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.optic.console.DebugUtil;
 import com.optic.console.application.service.AuthService;
 import com.optic.console.domain.user.dto.AuthResponse;
 import com.optic.console.domain.user.dto.LoginRequest;
 import com.optic.console.domain.user.dto.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
@@ -15,7 +19,9 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public void register(@RequestBody RegisterRequest request) {
+    public void register(@RequestBody RegisterRequest request)  throws Exception {
+        return DebugUtil.dd(request);
+        System.out.println("DDKDKD");
         authService.register(request);
     }
 
