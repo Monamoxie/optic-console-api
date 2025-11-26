16e75f846a8bd1e5401d4fbfa7f179eae5b06d5b
Mona Moxie <moxie4lyf@gmail.com>
2025-11-26 21:53:05 +0000

    fix auth response

--- src/main/java/com/optic/console/domain/user/dto/AuthResponse.java
+++ src/main/java/com/optic/console/domain/user/dto/AuthResponse.java
@@ -1,10 +1,17 @@
 package com.optic.console.domain.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
     private String token;
     private String email;
-    private String fullName;
+    private String firstName;
+    private String lastName;
 }
