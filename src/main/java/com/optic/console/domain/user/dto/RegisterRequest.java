9b22c078d47915120837459ea7bc4017f83acca7
Mona Moxie <moxie4lyf@gmail.com>
2025-12-03 20:32:18 +0000

    implemet register request

--- src/main/java/com/optic/console/domain/user/dto/RegisterRequest.java
+++ src/main/java/com/optic/console/domain/user/dto/RegisterRequest.java
@@ -1,10 +1,32 @@
 package com.optic.console.domain.user.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
+
 
 @Data
 public class RegisterRequest {
+    
+    @NotBlank(message = "Email is required")
+    @Email(message = "Please provide a valid email address")
+    @Size(max = 100, message = "Email must be less than 100 characters")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
+    @Pattern(
+        regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,100}$",
+        message = "Password must contain at least one digit, one lowercase, one uppercase letter and one special character"
+    )
     private String password;
-    private String fullName;
+
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
+    @Pattern(regexp = "^[a-zA-Z\\s'-]+", message = "First name can only contain letters, spaces, hyphens, and apostrophes")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
+    @Pattern(regexp = "^[a-zA-Z\\s'-]+", message = "Last name can only contain letters, spaces, hyphens, and apostrophes")
+    private String lastName;
 }
