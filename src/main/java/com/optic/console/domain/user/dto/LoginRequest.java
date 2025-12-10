c519c76f57ee2425808f321aa228c03f10112de4
Mona Moxie <moxie4lyf@gmail.com>
2025-12-10 21:26:27 +0000

    update login request validation rules

--- src/main/java/com/optic/console/domain/user/dto/LoginRequest.java
+++ src/main/java/com/optic/console/domain/user/dto/LoginRequest.java
@@ -1,9 +1,17 @@
 package com.optic.console.domain.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Please provide a valid email address")
+    @Size(max = 100, message = "Email must be less than 100 characters")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
 }
