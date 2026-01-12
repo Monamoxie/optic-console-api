60be5b9b247602e45eff1e8cbeb6fa1988ad0c1a
Mona Moxie <moxie4lyf@gmail.com>
2026-01-12 21:53:09 +0000

    update token generator

--- src/main/java/com/optic/console/application/TokenGenerator.java
+++ src/main/java/com/optic/console/application/TokenGenerator.java
@@ -7,10 +7,26 @@ import java.util.Base64;
 
 @Component
 public class TokenGenerator {
-    private final SecureRandom random = new SecureRandom();
+    private final SecureRandom random;
     private final Base64.Encoder encoder = Base64.getUrlEncoder().withoutPadding();
 
+    public TokenGenerator() {
+        this(new SecureRandom());
+    }
+
+    protected TokenGenerator(SecureRandom secureRandom) {
+        this.random = secureRandom;
+    }
+
+    protected SecureRandom createSecureRandom() {
+        return new SecureRandom();
+    }
+
     public String generate(int bytes) {
+        if (bytes <= 0) {
+            throw new IllegalArgumentException("Number of bytes must be positive");
+        }
+        
         byte[] buffer = new byte[bytes];
         random.nextBytes(buffer);
         return encoder.encodeToString(buffer);
