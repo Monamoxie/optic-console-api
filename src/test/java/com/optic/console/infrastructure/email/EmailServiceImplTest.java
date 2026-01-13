6616b5e7e63eaf97fd0330d27e24d10276c32a9b
Mona Moxie <moxie4lyf@gmail.com>
2026-01-13 20:32:57 +0000

    fixing missing imports in test file

--- src/test/java/com/optic/console/infrastructure/email/EmailServiceImplTest.java
+++ src/test/java/com/optic/console/infrastructure/email/EmailServiceImplTest.java
@@ -1,19 +1,22 @@
 package com.optic.console.infrastructure.email;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.thymeleaf.context.Context;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.thymeleaf.context.Context;
 
 @ExtendWith(MockitoExtension.class)
 class EmailServiceImplTest {
