a8270784b457bf3d5ada71c4871de7ebf608cb57
Mona Moxie <moxie4lyf@gmail.com>
2026-04-14 22:03:33 +0100

    wip: refacor role model

--- src/main/java/com/optic/console/domain/auth/Role.java
+++ src/main/java/com/optic/console/domain/auth/Role.java
@@ -1,13 +1,20 @@
 package com.optic.console.domain.auth;
 
+import com.optic.console.domain.workspace.Workspace;
 import jakarta.persistence.*;
 import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
-//@Entity
+@Entity
+@Getter
+@Setter
+@Builder
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,6 +22,16 @@ public class Role {
 
     @Column(nullable = false, unique = true, length = 100)
     private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String slug;
+
+    @Column(length = 50)
+    private String scope;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "workspace_id")
+    private Workspace workspace;
 
     @Column
     private String description;
